84673f7ea17e5e18cbc82916feaa85a29848bb54
Eugene Hodakov <e.hodakov@gmail.com>
2012-03-16 23:23:45 +0400

    Done with home work static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -42,4 +42,17 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should be right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
 end
